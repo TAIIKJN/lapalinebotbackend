e37556628b5241c724d3482e3cf147d64ac3264d
TAIIKJN <taii.kanjana@gmail.com>
2020-05-29 15:27:58 +0700

    444444444

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const line = require("@line/bot-sdk");
 const request = require("request");
+var moment = require('moment'); // require
+
 
 require("dotenv").config();
 
@@ -205,7 +207,7 @@ function handleMessageEvent(event) {
         var data = result[0];
         var nowyear = new Date().getFullYear();
         
-        console.log(data.BIRTH.toString());
+        console.log(moment(data.BIRTH).format() );
         console.log(data.BIRTH.split("-"));
         var age = parseInt(nowyear - data.BIRTH.split("-")[0]);
         var msg = {
