42d823bfec52da104f072797e99e1726de261edc
TAIIKJN <taii.kanjana@gmail.com>
2020-05-29 15:22:06 +0700

    22222222222

--- server.js
+++ server.js
@@ -205,7 +205,7 @@ function handleMessageEvent(event) {
         var data = result[0];
         var nowyear = new Date().getFullYear();
         
-
+        console.log(data.BIRTH);
         console.log(data.BIRTH.split("-"));
         var age = parseInt(nowyear - data.BIRTH.split("-")[0]);
         var msg = {
