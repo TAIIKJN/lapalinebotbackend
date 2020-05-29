e1a7246a6a5bac23014a17dfb7f51819ce0ad3ce
TAIIKJN <taii.kanjana@gmail.com>
2020-05-29 14:16:49 +0700

    55556

--- server.js
+++ server.js
@@ -196,17 +196,15 @@ function handleMessageEvent(event) {
       });
     });
 
-  } else if  (eventText==555){
-  //(eventText.split(" ")[0] == "search") {
-    // var query = `SELECT * FROM PERSON WHERE CID = '${eventText.split(" ")[1]}'`;
-    // console.log(query);
-    // con.connect(function (err) {
-    //   con.query(query, function (err, result, fields) {
-      
-    //     console.log(result);
-    //     var data = result[0];
-    //     var nowyear = new Date().getFullYear()
-    //     var age = parseInt(nowyear - data.BIRTH.split("-")[0])
+  } else if (eventText.split(" ")[0] == "search") {
+    var query = `SELECT * FROM PERSON WHERE CID = '${eventText.split(" ")[1]}'`;
+    console.log(query);
+    con.connect(function (err) {
+      con.query(query, function (err, result, fields) {
+        console.log(result);
+        var data = result[0];
+        var nowyear = new Date().getFullYear()
+        var age = parseInt(nowyear - data.BIRTH.split("-")[0])
         var msg = {
           "type": "flex",
           "altText": "Flex Message",
@@ -323,8 +321,8 @@ function handleMessageEvent(event) {
           }
         }
         return client.replyMessage(event.replyToken, msg);
-   //   });
-  //  });
+      });
+    });
   }
   //  else if (eventText === "ประวัติส่วนตัวของฉัน") {
   //   var msg = {
